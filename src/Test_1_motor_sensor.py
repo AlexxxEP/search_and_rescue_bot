8c0e73c5c20c42ad43c1d1aad3236ed4b4eca99c
AlexMens <93798983+AlexMens@users.noreply.github.com>
2023-11-27 16:59:47 +0100

    Update Test_1_motor_sensor.py

--- src/Test_1_motor_sensor.py
+++ src/Test_1_motor_sensor.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 # An EV3 Python (library v2) solution to Exercise 3
+
 # of the official Lego Robot Educator lessons that
+
 # are part of the EV3 education software
 
-from ev3dev2.motor import LargeMotor
+from ev3dev2.motor import Motor
+from ev3dev2.sensor.lego import UltrasonicSensor
 from ev3dev2.motor import MoveTank, OUTPUT_B, OUTPUT_C
 from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.button import Button
@@ -12,9 +15,13 @@ from time import sleep
 
 btn = Button() # we will use any button to stop script
 
-lm = LargeMotor()
+lm = Motor()
+
+us = UltrasonicSensor()
 
 while not btn.any():    # exit loop when any button pressed
 
-        lm.on_for_seconds(50, 3)
+    if us.distance_centimeters < 40:
+
+        lm.on(50)
         sleep(1)
