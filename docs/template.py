4b2f7800ca02193a9773bc62e797c907f9a62077
Alexandre EANG <alexandre.eang@outlook.com>
2023-11-28 20:20:20 +0100

    Added template py script for consistency

---
+++ docs/template.py
@@ -0,0 +1,46 @@
+# ---  ---  ---  ---  ---
+# file name:         % name of file + extension %
+# description :
+#
+#
+#
+# author:            % author first name + LAST NAME %
+# created on:        % YYYY MM DD of creation %
+# last updated:      % YYYY MM DD of last update %
+# updated by:        % last update author first name + LAST NAME %
+# comment :          * % comment 1 %
+#                    * % comment 2 %
+#                    * % long comment 3 %
+#                      % long comment 3 %
+# ---  ---   ---  ---  ---  ---
+#!/usr/bin/env python3
+# ---  ---  ---  ---  ---  ---
+
+
+
+# ---  IMPORTS  ---  ---  ---  ---
+# list imports here
+# ---  ---   ---  ---  ---  ---
+
+
+
+# ---  DECLARATIONS  ---
+    # --- CONSTANTS
+# list constants declarations here
+    # --- VARIABLES
+# list variable declarations here
+    # --- FUNCTIONS
+# instaciate functions here
+# ---  ---  ---  ---  ---  ---
+
+
+
+# ---  CODE START  ---
+# write main code here
+# ---  ---  ---  ---  ---  ---
+
+
+
+# ---  FUNCTION DEFINITION  ---
+# write function definitions here
+# ---  ---  ---  ---  ---  ---  ---
