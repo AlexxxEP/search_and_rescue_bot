e4b50785548d88af8dfd2acaf36d9de99898ba86
Alexandre EANG <alexandre.eang@outlook.com>
2023-11-29 11:49:51 +0100

    Modified main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,43 @@
-# ---  ---  ---
-# file name:    main.py
-# author:      Alexandre MENSAH
-# date:        2023 11 27
-# ---  ---  ---
+# ---  ---  ---  ---  ---
+# file name:         main.py
+# description :
+#
+#
+#
+# author:            Alexandre EANG
+# created on:        2023 11 8
+# last updated:      2023 11 29
+# updated by:        Alexandre EANG
+# comment :         
+# ---  ---   ---  ---  ---  ---
+#!/usr/bin/env python3
+# ---  ---  ---  ---  ---  ---
 
-# Hello
+
+
+# ---  IMPORTS  ---  ---  ---  ---
+# list imports here
+# ---  ---   ---  ---  ---  ---
+
+
+
+# ---  DECLARATIONS  ---
+    # --- CONSTANTS
+# list constants declarations here
+    # --- VARIABLES
+# list variable declarations here
+    # --- FUNCTIONS
+# instaciate functions here
+# ---  ---  ---  ---  ---  ---
+
+
+
+# ---  CODE START  ---
+# write main code here
+# ---  ---  ---  ---  ---  ---
+
+
+
+# ---  FUNCTION DEFINITION  ---
+# write function definitions here
+# ---  ---  ---  ---  ---  ---  ---
