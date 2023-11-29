756544aaa3cfccf6c76c349c8d41773321aeccd5
AlexxxEP <alexandre.eang@outlook.com>
2023-11-29 21:01:43 +0100

    Applied template to main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,48 @@
 # ---  ---  ---
 # file name:    main.py
-# author:      Alexandre MENSAH
+# author:      
 # date:        2023 11 27
 # ---  ---  ---
+# ---  ---  ---  ---  ---
+# file name:         % name of file + extension %
+# description :
+#
+#
+#
+# author:            Alexandre MENSAH
+# created on:        2023 11 27
+# last updated:      2023 11 29
+# updated by:        Alexandre EANG
+# comment :          * Applied template to this document
+# ---  ---   ---  ---  ---  ---
+#!/usr/bin/env python3
+# ---  ---  ---  ---  ---  ---
+
+
+
+# ---  IMPORTS  ---  ---  ---  ---
+# list imports here
+# ---  ---   ---  ---  ---  ---
+
+
+
+# ---  DECLARATIONS  ---
+    # --- CONSTANTS
+# list constants declarations here
+    # --- VARIABLES
+# list variable declarations here
+    # --- FUNCTIONS
+# instaciate functions here
+# ---  ---  ---  ---  ---  ---
+
+
+
+# ---  CODE START  ---
+# write main code here
+# ---  ---  ---  ---  ---  ---
+
+
+
+# ---  FUNCTION DEFINITION  ---
+# write function definitions here
+# ---  ---  ---  ---  ---  ---  ---
