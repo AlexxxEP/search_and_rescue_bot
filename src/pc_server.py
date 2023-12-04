b0240f6204878603796598ada1c20ca70a583083
zhipengze <82363635+zhipengze@users.noreply.github.com>
2023-12-04 16:21:29 +0100

    Update pc_server.py

--- src/pc_server.py
+++ src/pc_server.py
@@ -3,20 +3,17 @@ import socket
 
 server_port = 12345
 
-# 创建套接字
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('', server_port))
 server_socket.listen(1)
 
 print("Waiting for connection...")
 
-# 接受连接
 connection, client_address = server_socket.accept()
 print("Connected to", client_address)
 
 try:
     while True:
-        # 接收数据
         data = connection.recv(1024)
         if data:
             print("Received data:", data.decode('utf-8'))
