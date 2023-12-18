da39e82b016e0054fba2875e1686d42db8182a67
AlexxxEP <77839208+AlexxxEP@users.noreply.github.com>
2023-12-18 14:40:12 +0100

    Update claw.py

--- src/claw.py
+++ src/claw.py
@@ -2,16 +2,16 @@ from ev3dev2.motor import MediumMotor, OUTPUT_A, OUTPUT_B
 from time import sleep
 
 # Initialize the motors connected to the claw
-claw_motor_left = MediumMotor(OUTPUT_A)
+#claw_motor_left = MediumMotor(OUTPUT_A)
 claw_motor_right = MediumMotor(OUTPUT_B)
 
 # Define functions to control the claw movement
 def open_claw():
-    claw_motor_left.on_for_seconds(speed=50, seconds=1)
+    #claw_motor_left.on_for_seconds(speed=50, seconds=1)
     claw_motor_right.on_for_seconds(speed=-50, seconds=1)
 
 def close_claw():
-    claw_motor_left.on_for_seconds(speed=-50, seconds=1)
+    #claw_motor_left.on_for_seconds(speed=-50, seconds=1)
     claw_motor_right.on_for_seconds(speed=50, seconds=1)
 
 # Test the claw movement
@@ -24,5 +24,5 @@ try:
 
 except KeyboardInterrupt:
     # Stop motors and exit cleanly on Ctrl+C
-    claw_motor_left.off()
+    #claw_motor_left.off()
     claw_motor_right.off()
