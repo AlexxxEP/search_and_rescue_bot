d3dc15dd0065ed96c39e0f94a95549e2e75ec6e3
AlexxxEP <alexandre.eang@outlook.com>
2023-12-20 13:00:51 +0100

    Added init routine

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 '''input
 Y
-OUTPUT_A
-OUTPUT_D
+A
+C
 Y
-OUTPUT_A
-OUTPUT_D
+A
+D
 N
 '''
 
-USER_DEBUG = True
+USER_DEBUG = False
 
 def DEBUG(userinput):
     if (USER_DEBUG):
@@ -31,9 +31,9 @@ def DEBUG(userinput):
 #
 # author:            Alexandre MENSAH
 # created on:        2023 11 27
-# last updated:      2023 11 29
+# last updated:      2023 12 20
 # updated by:        Alexandre EANG
-# comment :          * Applied template to this document
+# comment :          * Setup event handling
 # ---  ---   ---  ---  ---  ---
 
 
@@ -50,10 +50,11 @@ from time import sleep
 
 
 # ---  DECLARATIONS  ---
-# --- CONSTANTS
-# list constants declarations here
+# --- CONSTANTS / CONFIG
+
 # --- VARIABLES
-# list variable declarations here
+ERR_port_claw = False
+ERR_port_wheels = False
 # ---  ---  ---  ---  ---  ---
 
 
@@ -68,90 +69,170 @@ def main():
     print("\nStarting program now ...\n")
 
 
-    # Checking port attribution
-    try:
-        wheels
-        wheels_port_error =False
-    except:
-        print("\t>>> Ports for wheeldrive have not been defined.")
-        wheels_port_error =True
-    try:
-        claw
-        claw_port_error =False
-    except:
-        print("\t>>> Port for claw has not been defined.")
-        claw_port_error =True
-
-
-    # Fix port attribution
-    if (wheels_port_error or claw_port_error):
+    # Checking connections
+    init_checkports = init_check()
+    if (ERR_port_wheels or ERR_port_claw):
         print("\nCheck connection on robot according to current configuration.")
         print("Robot configuration can be found under /config directory.")
-        print("For permanent changes, use init_config() to modify robot configuration.")
-
-    if (wheels_port_error):
-        userinput = input("\nSetup ports for wheeldrive now ? (Y: proceed / N: cancel)")
+        print("For permanent changes, use init_configure() to modify robot configuration.")
+    if (ERR_port_wheels):
+        userinput = input("\nSetup ports for wheeldrive now ? (Y: proceed / N: cancel)\n").upper()
         check = init_checkwheels(userinput)
         if (check == 1):
             return
-
-    if (claw_port_error):
-        userinput = input("\nSetup port for claw now ? (Y: proceed / N: cancel)")
+    if (ERR_port_claw):
+        userinput = input("\nSetup port for claw now ? (Y: proceed / N: cancel)\n").upper()
         check = init_checkclaw(userinput)
         if (check == 1):
             return
 
-    claw_open()
-    wheels.on(50,50)
+    #claw_open()
+    #wheels.on(50,50)
     return
 
 
-def init(left_wheel_port, right_wheel_port, claw_port):
+def init(config='user'):
     """
-    Initializes the ports of all actuators
-    left_wheel_port : output port of the corresponding actuator
-    right_wheel_port : output port of the corresponding actuator
-    claw_port : output port of the corresponding actuator
+    Initializes the software settings by importing config file.
+    By default, the config file used is the user config.
+    If the user wants to apply the default configuration, 
+    user may pass 'default' as argument.
+
+    Argument 'config':
+    - 'user'    :   initializes software with user config.
+                    when parameter can't be found, default
+                    configuration parameters will be used.
+
+    - 'default' :   initializes software with default config
+                    without updating user config.
+
+    - 'reset'   :   overwrite user config with default config,
+                    then intializes software with user config.
     """
-    wheels = MoveTank(left_wheel_port, right_wheel_port)
-    claw = MediumMotor(claw_port)
+    if (config != 'user' and config != 'default' and config != 'reset'):
+        print("\n\t>>> Wrong argument.")
+        return 1
+
+    global SYS_port_lwheel      
+    global SYS_port_rwheel      
+    global SYS_port_claw        
+    global SYS_phys_trackdist   
+    global SYS_phys_wheeldiameter
+    global SYS_targ_diameter    
+    global SYS_targ_color       
+    global SYS_envi_width       
+    global SYS_envi_length      
+    global SYS_envi_floorcolor  
+    global SYS_envi_edgecolor   
+    
+
+
+    with open('../config/config.default.json', 'r') as cfg_def_json:
+        cfg_def = json.load(cfg_def_json)
+        
+
+        if (config == 'user'):
+            with open('../config/config.user.json', 'r') as cfg_user_json:
+                cfg_user = json.load(cfg_user_json)
+                SYS_port_lwheel             = cfg_user['port']['lwheel']
+                SYS_port_rwheel             = cfg_user['port']['rwheel']
+                SYS_port_claw               = cfg_user['port']['claw']
+                SYS_phys_trackdist          = cfg_user['physical']['trackdist']
+                SYS_phys_wheeldiameter      = cfg_user['physical']['wheeldiameter']
+                SYS_targ_diameter           = cfg_user['target']['diameter']
+                SYS_targ_color              = cfg_user['target']['color']
+                SYS_envi_width              = cfg_user['environment']['width']
+                SYS_envi_length             = cfg_user['environment']['length']
+                SYS_envi_floorcolor         = cfg_user['environment']['floorcolor']
+                SYS_envi_edgecolor          = cfg_user['environment']['edgecolor']
+        elif (config == 'default'):
+            SYS_port_lwheel             = cfg_def['port']['lwheel']
+            SYS_port_rwheel             = cfg_def['port']['rwheel']
+            SYS_port_claw               = cfg_def['port']['claw']
+            SYS_phys_trackdist          = cfg_def['physical']['trackdist']
+            SYS_phys_wheeldiameter      = cfg_def['physical']['wheeldiameter']
+            SYS_targ_diameter           = cfg_def['target']['diameter']
+            SYS_targ_color              = cfg_def['target']['color']
+            SYS_envi_width              = cfg_def['environment']['width']
+            SYS_envi_length             = cfg_def['environment']['length']
+            SYS_envi_floorcolor         = cfg_def['environment']['floorcolor']
+            SYS_envi_edgecolor          = cfg_def['environment']['edgecolor']
+        elif (config == 'reset'):
+            with open('../config/config.user.json', 'w') as cfg_user_json:
+                json.dump(cfg_def,cfg_user_json, indent=2)
+            SYS_port_lwheel             = cfg_def['port']['lwheel']
+            SYS_port_rwheel             = cfg_def['port']['rwheel']
+            SYS_port_claw               = cfg_def['port']['claw']
+            SYS_phys_trackdist          = cfg_def['physical']['trackdist']
+            SYS_phys_wheeldiameter      = cfg_def['physical']['wheeldiameter']
+            SYS_targ_diameter           = cfg_def['target']['diameter']
+            SYS_targ_color              = cfg_def['target']['color']
+            SYS_envi_width              = cfg_def['environment']['width']
+            SYS_envi_length             = cfg_def['environment']['length']
+            SYS_envi_floorcolor         = cfg_def['environment']['floorcolor']
+            SYS_envi_edgecolor          = cfg_def['environment']['edgecolor']
+
+
+    # wheels = MoveTank(left_wheel_port, right_wheel_port)
+    # claw = MediumMotor(claw_port)
+
     return
     
-    
+
+def init_check():
+    """
+    """
+    global ERR_port_wheels
+    global ERR_port_claw
+    DEBUG(ERR_port_wheels)
+    try:
+        wheels
+        ERR_port_wheels =False
+    except:
+        print("\t>>> Ports for wheeldrive have not been defined.")
+        ERR_port_wheels =True
+    try:
+        claw
+        ERR_port_claw =False
+    except:
+        print("\t>>> Port for claw has not been defined.")
+        ERR_port_claw =True
+    return
+
+
+
 def init_checkwheels(userinput="Y"):
     """
     remediates un-initialized wheels port
     """
     DEBUG(userinput)
 
-    if (userinput == "N" or userinput == "n"):
+    if (userinput == "N" ):
         print("\n\nExiting ...")
         return 1
 
-    elif ( userinput == "Y" or userinput == "y"):
-        user_leftmotorport = input("\nRegister left motor port :\n")
+    elif (userinput == "Y" ):
+        user_leftmotorport = input("\nRegister left motor port : (A/B/C/D)\n").upper()
         DEBUG(user_leftmotorport)
-        user_rightmotorport = input("\nRegister right motor port :\n")
+        user_rightmotorport = input("\nRegister right motor port : (A/B/C/D)\n").upper()
         DEBUG(user_rightmotorport)
         try:
-            wheels = MoveTank(user_leftmotorport,user_rightmotorport)
+            wheels = MoveTank('ev3-ports:out'+user_leftmotorport,'ev3-ports:out'+user_rightmotorport)
         except Exception as error:
             print("\nAssignement of ports for wheeldrive was unsuccesful.\n\n")
             print("\t>>> ",error)
-            checkinput = input("\n\nWould you like to select different ports ? (Y/N)")
+            checkinput = input("\n\nWould you like to select different ports ? (Y/N)\n").upper()
             check = init_checkwheels(checkinput)
             return check
         else:
-            Print("Assignement of ports for wheeldrive succesful.")
+            print("Assignement of ports for wheeldrive succesful.")
             return 0
         return
 
-    else:
-        print("\nIncorrect argument.")
-        checkinput = input("Setup ports for wheeldrive now ? (Y: proceed / N: cancel)\n")
-        check = init_checkwheels(checkinput)
-        return check
-    return
+    print("\nIncorrect argument.")
+    checkinput = input("Setup ports for wheeldrive now ? (Y: proceed / N: cancel)\n").upper()
+    check = init_checkwheels(checkinput)
+    return check
     
     
 def init_checkclaw(userinput):
@@ -162,30 +243,30 @@ def init_checkclaw(userinput):
         print("\n\t> " + userinput) # just for seeing inputs in console (Alexandre EANG)
 
 
-    if (userinput == "N" or userinput == "n"):
+    if (userinput == "N"):
         print("\n\nExiting ...")
         return 1
 
-    elif ( userinput == "Y" or userinput == "y"):
-        user_clawport = input("\nRegister claw motor port :\n")
-        DEBUG(claw_port)
+    elif (userinput == "Y"):
+        user_clawport = input("\nRegister claw motor port : (A/B/C/D)\n").upper()
+        DEBUG(user_clawport)
 
         try:
-            claw = MediumMotor(user_clawport)
+            claw = MediumMotor('ev3-ports:out'+user_clawport)
         except Exception as error:
             print("\nAssignement of port for claw was unsuccesful.\n\n")
             print("\t>>> ",error)
-            checkinput = input("\n\nWould you like to select a different port ? (Y/N)")
+            checkinput = input("\n\nWould you like to select a different port ? (Y/N)\n").upper()
             check = init_checkclaw(checkinput)
             return check
         else:
-            Print("Assignement of port for claw succesful.")
+            print("Assignement of port for claw succesful.")
             return 0
         return
 
     else:
         print("\nIncorrect argument.")
-        checkinput = input("Setup port for claw now ? (Y: proceed / N: cancel)\n")
+        checkinput = input("Setup port for claw now ? (Y: proceed / N: cancel)\n").upper()
         DEBUG(checkinput)
         check = init_checkclaw(checkinput)
         return check
@@ -195,7 +276,19 @@ def init_checkclaw(userinput):
 
 
 # ---  CODE START  ---  ---
+init()
+
 main()
+
+
+
+
+
+
+
+
+
+
 # with open('../config/config.default.json','r') as cfg:
 #     prout = json.load(cfg)["ports"]
 
