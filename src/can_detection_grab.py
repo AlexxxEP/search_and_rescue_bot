3f01521b40ec7625c15c8aa099db764a5cf6c58a
123Cxsen <90620731+123Cxsen@users.noreply.github.com>
2024-01-08 00:43:48 +0100

    Update can_detection_grab.py

--- src/can_detection_grab.py
+++ src/can_detection_grab.py
@@ -58,6 +58,7 @@ if nr_blocks >= 1:
     sleep(10)
     try:
         while 1:
+            nr_blocks, blocks = pixy2.get_blocks(1, 1)
             x = blocks[0].x_center
             distance = us.value()
             print('The x-axis of the block is: ', x)
