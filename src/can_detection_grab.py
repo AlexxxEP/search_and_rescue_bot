3c59e9d22e090605d2ba0d842ea6b419068a7452
123Cxsen <90620731+123Cxsen@users.noreply.github.com>
2024-01-08 00:19:14 +0100

    Update can_detection_grab.py

--- src/can_detection_grab.py
+++ src/can_detection_grab.py
@@ -65,6 +65,7 @@ if nr_blocks >= 1:
                 sleep(2)
                 open_claw()
                 sleep(2)
+                break
     except KeyboardInterrupt:
         # Stop motors and exit cleanly on Ctrl+C
         #claw_motor_left.off()
